956cd6dfe9ae6098cc9c07dc5ec899a1cbdc456d
Shaamallow <eyal.benaroche@gmail.com>
2023-12-02 23:48:36 +0100

    Day2 - puzzle 2

--- day2/main.py
+++ day2/main.py
@@ -5,7 +5,6 @@ doc = open("input.txt", "r")
 lines = doc.readlines()
 
 cubes_dict = {"red": 12, "green": 13, "blue": 14}
-id = 0
 
 
 def check_game(line):
@@ -39,4 +38,34 @@ def puzzle_1(lines):
     return id_sum
 
 
+def puzzle_2(lines):
+    sum = 0
+    for line in lines:
+        cubes_dict = {"red": 0, "green": 0, "blue": 0}
+        line = line.strip().split("\n")[0]
+        # Get 1 game
+        line = line.split(":")
+        sets = line[1].split(";")
+
+        for turns in sets:
+            turns = turns.split(",")
+            turns = [turn.strip() for turn in turns]
+            cubes = []  # number of cube i
+            colors = []  # color of cube i
+
+            for turn in turns:
+                turn = turn.split(" ")
+                cubes.append(int(turn[0]))
+                colors.append(turn[1])
+
+            for i in range(len(cubes)):
+                if cubes[i] > cubes_dict[colors[i]]:
+                    cubes_dict[colors[i]] = cubes[i]
+
+        sum += cubes_dict["red"] * cubes_dict["green"] * cubes_dict["blue"]
+
+    return sum
+
+
 print(puzzle_1(lines))
+print(puzzle_2(lines))
